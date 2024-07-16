a3dc109ac56e8713d222f0340fda37a17424b3cb
Rakemoon <rakemoonkuzmin@gmail.com>
2024-07-16 22:45:40 +0700

    feat: Adding navigation bar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import IconHome from "@tabler/icons-svelte/icons/home";
+  import IconCart from "@tabler/icons-svelte/icons/shopping-cart"
+  interface NavbarProp {
+    currentPath?: string
+  }
+
+  interface Page {
+    path: string;
+    label: string;
+    icon: typeof IconHome
+  }
+
+  let { currentPath }: NavbarProp = $props();
+
+  const pages: Page[] = [
+    {
+      path: "/",
+      label: "Home",
+      icon: IconHome
+    },
+    {
+      path: "/cart",
+      label: "Cart",
+      icon: IconCart
+    }
+  ];
+</script>
+
+<div>
+  <ul class="nav-list">
+    {#each pages as page }
+      <li class="nav-item" aria-current={currentPath === page.path ? "page" : undefined}>
+        <a href={page.path}>
+          <svelte:component this={page.icon} size={20} stroke={1}></svelte:component>
+          <span>{page.label}</span>
+        </a>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .nav-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 25%;
+    height: 100%;
+    position: fixed;
+    overflow: true;
+    background-color: #f1f1f1;
+  }
+
+  .nav-item a {
+    text-decoration: none;
+    display: flex;
+    color: black;
+    padding: 8px 16px;
+    align-items: center;
+  }
+
+  .nav-item span {
+    padding-left:4px;
+  }
+
+  .nav-item:hover {
+    background-color: darkgrey;
+  }
+
+  .nav-item[aria-current=page] {
+    background: greenyellow;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte";
+  import { page } from "$app/stores";
+  let { children } = $props();
+</script>
+
+<Navbar currentPath={$page.route.id!}/>
+<div class="route-display">
+  {@render children()}
+</div>
+
+<style>
+  .route-display {
+    margin-left: 25%;
+    padding: 1px 16px;
+    height: 1000px;
+  }
+</style>
