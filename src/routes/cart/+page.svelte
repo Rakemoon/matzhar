47fd98de382e902294f38dca7ad6dc5e96a14747
Rakemoon <rakemoonkuzmin@gmail.com>
2024-07-17 09:51:41 +0700

    feat: Add plus minus button

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
-  import { CartState } from "$lib/states/CartState.svelte";
-    import { currencyFormat } from "$lib/util/string";
+  import { CartState, type CartItem } from "$lib/states/CartState.svelte";
+  import { currencyFormat } from "$lib/util/string";
   import products from "$products";
+
+  function addAmount(name: string) {
+    CartState.find(x => x.name === name)!.amount++;
+  }
+
+  function decreaseAmount(name: string) {
+    const index = CartState.findIndex(x => x.name === name)!;
+    CartState[index].amount = Math.max(0, CartState[index].amount - 1);
+    if (CartState[index].amount === 0) CartState.splice(index, 1);
+  }
+
+  function listenInput
+  (event: KeyboardEvent & { currentTarget: EventTarget & HTMLInputElement }, item: CartItem)
+  : void {
+    if (event.key !== "Enter") return;
+    const value = parseInt(event.currentTarget.value);
+    event.currentTarget.value = "";
+    if (isNaN(value)) return;
+    item.amount = Math.max(0, value);
+  }
 </script>
 
 <h1>Cart</h1>
@@ -13,6 +33,9 @@
         <p>Amount: {cart.amount}</p>
         <p>Price: {currencyFormat(cartdata.price)}</p>
         <p>Total: {currencyFormat(cartdata.price * cart.amount)}</p>
+        <button onclick={() => addAmount(cart.name)}>+</button>
+        <button onclick={() => decreaseAmount(cart.name)}>-</button>
+        <input onkeydowncapture={ev => listenInput(ev, cart)}/>
     </li>
   {/each}
 </ul>
